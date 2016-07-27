48e185ca9dc3e05bac2f2281b6116392b14d9757
mb3333mb@163.com <mb840720mb>
2016-07-27 18:30:33 +0800

    全部提交

---
+++ src/main/java/com/ai/platform/modules/cms/dao/GuestbookDao.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright &copy; 2012-2016 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
+package com.ai.platform.modules.cms.dao;
+
+import com.ai.platform.common.persistence.CrudDao;
+import com.ai.platform.common.persistence.annotation.MyBatisDao;
+import com.ai.platform.modules.cms.entity.Guestbook;
+
+/**
+ * 留言DAO接口
+ * @author ThinkGem
+ * @version 2013-8-23
+ */
+@MyBatisDao
+public interface GuestbookDao extends CrudDao<Guestbook> {
+
+}
