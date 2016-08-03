97bb5a71cd6829b7641c69f7cb7b9ba637c65193
jiaxiaoshuo <jiaxs@asiainfo.com>
2016-08-03 14:51:05 +0800

    工程路径修改

--- general-common-core/src/test/java/com/ai/slp/common/test/dubbo/loadconfig/LoadConfigTest.java
+++
@@ -1,9 +0,0 @@
-package com.ai.slp.common.test.dubbo.loadconfig;
-
-public class LoadConfigTest {
-	public static void main(String[] args) {
-		/*System.out.println("开始导入配置。。。。");
-		Load2ConfigCenter.main(new String[]{"g:\\dbload"});
-		System.out.println("完成导入配置。。。。");*/
-	}
-}
