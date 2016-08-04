b0c74aa339699e925f43dcb825f7c96e6c2b3d17
jiaxiaoshuo <jiaxs@asiainfo.com>
2016-08-04 11:18:04 +0800

    接口整理

--- general-common-api/src/main/java/com/ai/platform/common/api/sysuser/interfaces/ISysUserQuerySV.java
+++ general-common-api/src/main/java/com/ai/platform/common/api/sysuser/interfaces/ISysUserQuerySV.java
@@ -1,5 +1,11 @@
 package com.ai.platform.common.api.sysuser.interfaces;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
 import com.ai.platform.common.api.sysuser.param.SysUserListQueryRequest;
@@ -9,6 +15,9 @@ import com.ai.platform.common.api.sysuser.param.SysUserQueryResponse;
 import com.ai.platform.common.api.sysuser.param.SysUserThemeRequest;
 import com.ai.platform.common.api.sysuser.param.SysUserThemeResponse;
 
+@Path("/userservice")
+@Consumes({ MediaType.APPLICATION_JSON })
+@Produces({ MediaType.APPLICATION_JSON, MediaType.TEXT_XML })
 public interface ISysUserQuerySV {
 	/**
 	 * 查询用户信息
@@ -18,7 +27,10 @@ public interface ISysUserQuerySV {
 	 * @throws SystemException
 	 * @author zhanglh
 	 * @ApiCode PLAT_001
+	 * @RestRelativeURL userservice/queryUserInfo
 	 */
+	@POST
+	@Path("/queryUserInfo")
 	SysUserQueryResponse queryUserInfo(SysUserQueryRequest request)throws BusinessException,SystemException;
 	/**
 	 * 查询用户主题
@@ -28,7 +40,10 @@ public interface ISysUserQuerySV {
 	 * @throws SystemException
 	 * @author zhanglh
 	 * @ApiCode PLAT_002
+	 * @RestRelativeURL userservice/queryUserTheme
 	 */
+	@POST
+	@Path("/queryUserTheme")
 	SysUserThemeResponse queryUserTheme(SysUserThemeRequest request)throws BusinessException,SystemException;
 	
 	/**
@@ -39,6 +54,9 @@ public interface ISysUserQuerySV {
 	 * @throws SystemException
 	 * @author zhanglh
 	 * @ApiCode	PLAT_003
+	 * @RestRelativeURL userservice/queryUserByOfficeId
 	 */
+	@POST
+	@Path("/queryUserByOfficeId")
 	SysUserListQueryResponse queryUserByOfficeId(SysUserListQueryRequest request)throws BusinessException,SystemException;
 }
--- general-common-api/src/main/java/com/ai/platform/common/api/waitjobs/interfaces/ISysWaitjobsMangeSV.java
+++ general-common-api/src/main/java/com/ai/platform/common/api/waitjobs/interfaces/ISysWaitjobsMangeSV.java
@@ -1,5 +1,11 @@
 package com.ai.platform.common.api.waitjobs.interfaces;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
 import com.ai.opt.base.vo.BaseResponse;
@@ -7,6 +13,9 @@ import com.ai.platform.common.api.waitjobs.param.WaitjobsCompleteRequset;
 import com.ai.platform.common.api.waitjobs.param.WaitjobsInsertRequest;
 import com.ai.platform.common.api.waitjobs.param.WaitjobsInsertResponse;
 
+@Path("/waitjobsservice")
+@Consumes({ MediaType.APPLICATION_JSON })
+@Produces({ MediaType.APPLICATION_JSON, MediaType.TEXT_XML })
 public interface ISysWaitjobsMangeSV {
 	
 	/**
@@ -16,7 +25,10 @@ public interface ISysWaitjobsMangeSV {
 	 * @throws BusinessException,SystemException
 	 * @author jiaxs
 	 * @ApiCode PLAT_008
+	 * @RestRelativeURL waitjobsservice/insertWaitjobs
 	 */
+	@POST
+	@Path("/insertWaitjobs")
 	WaitjobsInsertResponse insertWaitjobs(WaitjobsInsertRequest insertRequest) throws BusinessException,SystemException;
 
 	/**
@@ -26,6 +38,9 @@ public interface ISysWaitjobsMangeSV {
 	 * @throws BusinessException,SystemException
 	 * @author jiaxs
 	 * @ApiCode PLAT_009
+	 * @RestRelativeURL waitjobsservice/completeWaitjobs
 	 */
+	@POST
+	@Path("/completeWaitjobs")
 	BaseResponse completeWaitjobs(WaitjobsCompleteRequset completeRequest) throws BusinessException,SystemException;
 }
