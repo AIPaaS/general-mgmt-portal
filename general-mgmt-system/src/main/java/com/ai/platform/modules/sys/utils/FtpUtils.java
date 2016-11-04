138e3ddb42516d0a67fcde446af229cc0ec56d22
meteor <zhouxh7@asiainfo.com>
2016-11-04 14:10:05 +0800

    ftp设置

--- general-mgmt-system/src/main/java/com/ai/platform/modules/sys/utils/FtpUtils.java
+++ general-mgmt-system/src/main/java/com/ai/platform/modules/sys/utils/FtpUtils.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPClientConfig;
 import org.apache.commons.net.ftp.FTPFile;
 import org.apache.log4j.Logger;
 
@@ -26,18 +27,20 @@ import com.ai.platform.common.utils.DateUtils;
 public class FtpUtils {
 	private static final Logger LOG = Logger.getLogger(FtpUtils.class);
 	private FTPClient ftpClient;
+
 	/**
 	 * init ftp servere
 	 */
 	public FtpUtils() {
-		LOG.error("开始读取ftp配置信息，当前时间戳："+DateUtils.getDateTime());
+		LOG.error("开始读取ftp配置信息，当前时间戳：" + DateUtils.getDateTime());
 		String ip = Global.getConfig("ftp.ip"); // 服务器IP地址
 		String userName = Global.getConfig("ftp.userName"); // 用户名
 		String userPwd = Global.getConfig("ftp.userPwd"); // 密码
-		int port =Integer.parseInt(Global.getConfig("ftp.port")); // 端口号
-	    String path = Global.getConfig("ftp.path"); // 读取文件的存放目录
-	    LOG.error("FTP信息|ip:"+ip+"|userName:"+userName+"|userPwd:"+userPwd+"|port:"+port+"|path:"+path);
-	    this.connectServer(ip, port, userName, userPwd, path);
+		int port = Integer.parseInt(Global.getConfig("ftp.port")); // 端口号
+		String path = Global.getConfig("ftp.path"); // 读取文件的存放目录
+		LOG.error(
+				"FTP信息|ip:" + ip + "|userName:" + userName + "|userPwd:" + userPwd + "|port:" + port + "|path:" + path);
+		this.connectServer(ip, port, userName, userPwd, path);
 	}
 
 	/**
@@ -51,25 +54,30 @@ public class FtpUtils {
 	 *             function:连接到服务器
 	 */
 	public void connectServer(String ip, int port, String userName, String userPwd, String path) {
-		LOG.error("开始连接ftp，当前时间戳："+DateUtils.getDateTime());
+		LOG.error("开始连接ftp，当前时间戳：" + DateUtils.getDateTime());
 		ftpClient = new FTPClient();
 		try {
+			FTPClientConfig config = new FTPClientConfig(FTPClientConfig.SYST_NT);
+			ftpClient.configure(config);
 			// 连接
 			ftpClient.connect(ip, port);
 			// 登录
 			ftpClient.login(userName, userPwd);
+			ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+			ftpClient.setControlEncoding("GBK");
+			ftpClient.enterLocalPassiveMode();
 			if (path != null && path.length() > 0) {
 				// 跳转到指定目录
 				ftpClient.changeWorkingDirectory(path);
 			}
 		} catch (SocketException e) {
-			LOG.error("连接ftp失败，当前时间戳："+DateUtils.getDateTime());
+			LOG.error("连接ftp失败，当前时间戳：" + DateUtils.getDateTime());
 			e.printStackTrace();
 		} catch (IOException e) {
-			LOG.error("连接ftp失败，当前时间戳："+DateUtils.getDateTime());
+			LOG.error("连接ftp失败，当前时间戳：" + DateUtils.getDateTime());
 			e.printStackTrace();
 		}
-		LOG.error("连接ftp成功，当前时间戳："+DateUtils.getDateTime());
+		LOG.error("连接ftp成功，当前时间戳：" + DateUtils.getDateTime());
 	}
 
 	/**
@@ -93,40 +101,40 @@ public class FtpUtils {
 	 * @throws IOException
 	 */
 	public List<String> getFileList() {
-		LOG.error("开始读取ftp文件，当前时间戳："+DateUtils.getDateTime());
+		LOG.error("开始读取ftp文件，当前时间戳：" + DateUtils.getDateTime());
 		List<String> fileLists = new ArrayList<String>();
 		// 获得指定目录下所有文件名
 		FTPFile[] ftpFiles = null;
 		try {
 			ftpFiles = ftpClient.listFiles(Global.getConfig("ftp.path"));
-			LOG.error("ftpFiles："+ftpFiles.length+"，当前时间戳："+DateUtils.getDateTime());
+			LOG.error("ftpFiles：" + ftpFiles.length + "，当前时间戳：" + DateUtils.getDateTime());
 		} catch (IOException e) {
 			e.printStackTrace();
-			LOG.error("读取ftp文件异常,当前时间戳："+DateUtils.getDateTime());
+			LOG.error("读取ftp文件异常,当前时间戳：" + DateUtils.getDateTime());
 			LOG.error(e.getMessage());
 		}
 		for (int i = 0; ftpFiles != null && i < ftpFiles.length; i++) {
 			FTPFile file = ftpFiles[i];
 			if (file.isFile()) {
 				fileLists.add(file.getName());
-				LOG.error("ftp文件名称："+file.getName()+"，当前时间戳："+DateUtils.getDateTime());
+				LOG.error("ftp文件名称：" + file.getName() + "，当前时间戳：" + DateUtils.getDateTime());
 			}
 		}
 		return fileLists;
 	}
-	
+
 	public InputStream readFile(String fileName) throws ParseException {
-		InputStream ins=null;
+		InputStream ins = null;
 		try {
 			// 从服务器上读取指定的文件
-		    ins = ftpClient.retrieveFileStream(fileName);
+			ins = ftpClient.retrieveFileStream(fileName);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return ins;
 	}
 
-	public void completePendingCommand(){
+	public void completePendingCommand() {
 		try {
 			ftpClient.completePendingCommand();
 		} catch (IOException e) {
@@ -145,14 +153,14 @@ public class FtpUtils {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		String ip = "111.9.116.182"; // 服务器IP地址
 		String userName = "test"; // 用户名
 		String userPwd = "123456"; // 密码
-		int port =21; // 端口号
-	    String path = "/home/test"; // 读取文件的存放目录
-	    FTPClient  ftpClient = new FTPClient();
+		int port = 21; // 端口号
+		String path = "/home/test"; // 读取文件的存放目录
+		FTPClient ftpClient = new FTPClient();
 		try {
 			// 连接
 			ftpClient.connect(ip, port);
@@ -174,9 +182,10 @@ public class FtpUtils {
 				FTPFile file = ftpFiles[i];
 				if (file.isFile()) {
 					fileLists.add(file.getName());
+					System.out.println(file.getName());
 				}
 			}
-			for(String file: fileLists){
+			for (String file : fileLists) {
 				InputStream ins = ftpClient.retrieveFileStream(file);
 				ins.close();
 				ftpClient.completePendingCommand();
