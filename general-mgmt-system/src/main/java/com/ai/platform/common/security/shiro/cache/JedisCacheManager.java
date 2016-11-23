8c37b6fc62974405fdac4576fa9f3d23192b4c1d
mengbo@asiainfo.com <mengbo@asiainfo.com>
2016-11-23 21:07:21 +0800

    session

--- general-mgmt-system/src/main/java/com/ai/platform/common/security/shiro/cache/JedisCacheManager.java
+++ general-mgmt-system/src/main/java/com/ai/platform/common/security/shiro/cache/JedisCacheManager.java
@@ -29,7 +29,7 @@ import com.google.common.collect.Sets;
  */
 public class JedisCacheManager implements CacheManager {
 
-	private String cacheKeyPrefix = "AIOPT_JSESSIONID";
+	private String cacheKeyPrefix = "shiro_cache_";
 	private static final String cachens=UniSessionUtil.getSessionPassNameSpace();
 
 	
