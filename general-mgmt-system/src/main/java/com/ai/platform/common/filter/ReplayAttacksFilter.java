d610e3376d0d942e2c718b75065f63f0808c37a3
meteor <zhouxh7@asiainfo.com>
2016-12-12 15:41:05 +0800

    重放攻击修改

--- general-mgmt-system/src/main/java/com/ai/platform/common/filter/ReplayAttacksFilter.java
+++ general-mgmt-system/src/main/java/com/ai/platform/common/filter/ReplayAttacksFilter.java
@@ -1,8 +1,6 @@
 package com.ai.platform.common.filter;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 import java.util.UUID;
 
 import javax.servlet.Filter;
@@ -19,25 +17,13 @@ import javax.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.ai.net.xss.util.CollectionUtil;
-import com.ai.net.xss.util.StringUtil;
 import com.ai.platform.modules.sys.utils.UserUtils;
-import com.alibaba.fastjson.JSON;
 
 public class ReplayAttacksFilter implements Filter {
 	private static Logger log = LoggerFactory.getLogger(ReplayAttacksFilter.class);
-	private List<String> ignorePathList;
 	public static final String USER_TOKEN_KEY = "user_token_key";
 
 	public void init(FilterConfig filterConfig) throws ServletException {
-		log.info("Replay Attacks fiter [ReplayAttacksFilter] init start ...");
-		String ignorePaths = filterConfig.getInitParameter("ignorePath");
-		if (!(StringUtil.isBlank(ignorePaths))) {
-			String[] ignorePathArr = ignorePaths.split(",");
-			this.ignorePathList = Arrays.asList(ignorePathArr);
-		}
-		log.info("ignorePathList=" + JSON.toJSONString(this.ignorePathList));
-		log.info("Replay Attacks fiter [ReplayAttacksFilter] init end");
 	}
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
@@ -51,16 +37,7 @@ public class ReplayAttacksFilter implements Filter {
 		HttpServletResponse res = (HttpServletResponse) response;
 		String uriPath = ((HttpServletRequest) request).getRequestURI();
 		HttpSession session = req.getSession();
-//		String contextPath = req.getContextPath() + "/";
-		if (isIgnorePath(uriPath)) {
-			log.info("ignore ReplayAttacksfilter,path[" + uriPath + "] pass through ReplayAttacksFilter, go ahead...");
-			chain.doFilter(request, response);
-			return;
-		}
-//		if (contextPath.equals(uriPath)) {
-//			log.info(
-//					"ignore ReplayAttacksfilter,path[" + uriPath + "] pass through ReplayAttacksFilter, go ahead...");
-//			setToken(res, req);
+//		if (!uriPath.contains("/home/")) {
 //			chain.doFilter(request, response);
 //			return;
 //		}
@@ -114,23 +91,5 @@ public class ReplayAttacksFilter implements Filter {
 
 	public void destroy() {
 		log.info("Replay Attacks fiter [ReplayAttacksFilter] destroy");
-	}
-
-	private boolean isIgnorePath(String servletPath) {
-		if (StringUtil.isBlank(servletPath)) {
-			return true;
-		}
-		if (CollectionUtil.isEmpty(this.ignorePathList)) {
-			return false;
-		}
-
-		for (String ignorePath : this.ignorePathList) {
-			if ((!(StringUtil.isBlank(ignorePath))) && (servletPath.contains(ignorePath.trim()))) {
-				return true;
-			}
-
-		}
-
-		return false;
 	}
 }
